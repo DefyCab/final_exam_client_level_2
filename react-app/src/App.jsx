c1d8d37e900fb3cf0683bcf9d0aa227dfa5a3b3b
defycab <love.bellinder@gmail.com>
2022-02-17 15:24:10 +0100

    started implementing functionality redirections

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import Workshops from "./components/Workshops";
 import "./css/app.css";
+
 const App = () => {
   return (
     <>
       <NavigationBar />
+      <Workshops />
+      <Routes>
+        <Route path="/" elements={<App />} />
+        <Route path="/categories/workshops" elements={<Workshops />} />
+      </Routes>
     </>
   );
 };
--- react-app/src/components/NavigationBar.jsx
+++ react-app/src/components/NavigationBar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h1 data-cy="main-header">COURSE LIST</h1>
       <div id="container">
-        <button data-cy="workshops">Workshops</button>
+        <button
+          onClick={() => navigate("/categories/workshops")}
+          data-cy="workshops"
+        >
+          Workshops
+        </button>
         <button data-cy="testing">Testing</button>
         <button data-cy="vue">Vue</button>
         <button data-cy="productivity">Productivity</button>
